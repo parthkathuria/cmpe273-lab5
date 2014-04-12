67dbfce217a00e1b5728da25afc05959a9f5123e
Parth Kathuria <parthkathuria09@gmail.com>
2014-04-12 14:53:52 -0700

    Final commit 1

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,31 @@
+$(document).ready(function(){
+    var items = $('td[title="status"]');
+    for( i=0; i<items.length; i++ ){
+            var status = items[i].innerHTML;
+            var isbn = items[i].id.slice("6");
+            var buttonID = "button#"+isbn;
+            if(status == "lost")
+                    {
+                            $(buttonID).attr("disabled",true);
+                    }
+            else{
+                    $(buttonID).removeAttr("disabled");
+            }
+    }
+    
+});
+
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+    var isbn = this.id;
+    var status = "{{status}}";
+    //alert('About to report lost on ISBN ' + isbn);
+    if(confirm('About to report lost on ISBN ' + isbn + '\nConfirm?')){
+    	$.ajax({
+        	url: "/library/v1/books/"+isbn+"?status=lost",
+        	type: "PUT",
+        	contentType: "application/json",
+        });
+	    window.location.reload(true);
+    }
+        
 });
